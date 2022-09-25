4db71a1649e684aeca4e3ff86a3c149781f22873
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2022-09-25 22:08:21 +0530

    Address Book Program

---
+++ Example.java
@@ -0,0 +1,9 @@
+package AddressBook;
+import java.util.*;
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book ");
+    }
+}
